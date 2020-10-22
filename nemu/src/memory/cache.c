643b56dbb44ebd5594dbeff56ab1711e0aa2d44e
tracer-ics2016 <tracer@njuics.org>
2020-10-22 14:28:33 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:28:33 up 11 days,  5:25,  1 user,  load average: 1.59, 1.37, 1.05 1e53338f8adc9ff4fff62d0d2bae66b6dec5e52

---
+++ nemu/src/memory/cache.c
@@ -0,0 +1,20 @@
+#include "common.h"
+#include <stdlib.h>
+#include "burst.h"
+#include "cpu/reg.h"
+#include "../../../lib-common/x86-inc/cpu.h"
+#define BLOCK_SIZE 64
+#define STORAGE_SIZE_L1 64*1024
+#define STORAGE_SIZE_L2 4*1024*1024
+#define EIGHT_WAY 8
+#define SIXTEEN_WAY 16
+uint32_t dram_read(hwaddr_t, size_t);
+void dram_write(hwaddr_t, size_t, uint32_t);
+void ddr3_read(hwaddr_t, void*);
+void ddr3_write(hwaddr_t, void*,uint8_t*);
+lnaddr_t seg_translate(swaddr_t, size_t, uint8_t);
+hwaddr_t page_translate(lnaddr_t);
+int is_mmio(hwaddr_t);
+uint32_t mmio_read(hwaddr_t, size_t, int);
+void mmio_write(hwaddr_t, size_t, uint32_t, int);
+CPU_state cpu;

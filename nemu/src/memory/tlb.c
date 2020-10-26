65d42a011c9e2355f1e887f4d9a1e9049df3afc5
tracer-ics2016 <tracer@njuics.org>
2020-10-26 12:56:47 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:56:47 up 15 days,  3:54,  1 user,  load average: 1.57, 1.35, 1.18 11a887ac41aea39f92775f3db78be1411b241b3c

--- nemu/src/memory/tlb.c
+++ nemu/src/memory/tlb.c
@@ -1,18 +1,18 @@
 #include "memory/tlb.h"
 
 uint32_t readTLB(uint32_t tg) {
-	// int i;
-	// for(i = 0; i < TLB_SIZE; i++) {
-	// 	if(tlb[i].valid && tlb[i].tag == tg) return tlb[i].page;
-	// }
-	 return -1;//invalid
+	int i;
+	for(i = 0; i < TLB_SIZE; i++) {
+		if(tlb[i].valid && tlb[i].tag == tg) return tlb[i].page;
+	}
+	return -1;//invalid
 }
 
 void resetTLB() {
-	// int i;
-	// for(i = 0; i < TLB_SIZE; i++) {
-	// 	tlb[i].valid = false;
-	// }
+	int i;
+	for(i = 0; i < TLB_SIZE; i++) {
+		tlb[i].valid = false;
+	}
 }
 
 void writeTLB(uint32_t tg, uint32_t page) {

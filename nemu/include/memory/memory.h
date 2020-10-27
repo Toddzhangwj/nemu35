d4b2aa11d472c76d558a9b41bbd8d4b7d6c18a5b
tracer-ics2016 <tracer@njuics.org>
2020-10-27 11:30:14 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:30:14 up 16 days,  2:27,  1 user,  load average: 1.39, 0.73, 0.59 b9c20072a2172c74e50f99f7075d781cfe42aa

--- nemu/include/memory/memory.h
+++ nemu/include/memory/memory.h
@@ -22,7 +22,7 @@ uint32_t lnaddr_read(lnaddr_t, size_t);
 uint32_t hwaddr_read(hwaddr_t, size_t);
 void swaddr_write(swaddr_t, size_t, uint32_t, uint8_t);
 void lnaddr_write(lnaddr_t, size_t, uint32_t);
-//void hwaddr_write(hwaddr_t, size_t, uint32_t);
+void hwaddr_write(hwaddr_t, size_t, uint32_t);
 
 void loadSregCache(uint8_t sreg);
 hwaddr_t page_translate(lnaddr_t addr, size_t len);
